80426f3b2c76ab2dd7cb3c08a46d4dae86d3bf37
dfldiego <dfl.diego92@gmail.com>
2020-09-29 11:40:17 -0300

    landingPage. seccion pasos a seguir. parte2.

--- cliente/src/components/Nosotros/Nosotros.jsx
+++ cliente/src/components/Nosotros/Nosotros.jsx
@@ -7,20 +7,20 @@ const Nosotros = () => {
         <Fragment>
             <section className="contenedor seccion">
                 <h2 class="fw-300 centrar-texto">Pasos a Seguir</h2>
-                <div className="iconos-nosotros">
-                    <div className="icono">
+                <div className="row">
+                    <div className="col-3">
                         <img src={require('../../assets/img/comida-rapida.svg')} alt="Icono elegi tu comida" />
                         <h3>1. Elegi tu comida</h3>
                         <p>Investigá a través de las distintas categorías de comida y elegí lo que mas te guste.</p>
                     </div>
 
-                    <div className="icono">
+                    <div className="col-3">
                         <img src={require('../../assets/img/dinero.svg')} alt="Icono hace tu pedido" />
                         <h3>2. Hacé tu pedido</h3>
                         <p>Es fácil y rápido. Una vez que dicidiste tu comida, podés pagar en la entrega o venir a retirarlo al local.</p>
                     </div>
 
-                    <div className="icono">
+                    <div className="col-3">
                         <img src={require('../../assets/img/moto.svg')} alt="Icono recibi tu pedido" />
                         <h3>3. Recibí tu pedido</h3>
                         <p>Nosotros te entregamos el pedido en tu puerta o te esperamos a que busques en nuestro local.</p>
