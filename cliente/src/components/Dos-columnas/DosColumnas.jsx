144a0459308b799b4234296a8ce679542a165641
dfldiego <dfl.diego92@gmail.com>
2020-09-29 13:58:30 -0300

    landingPage. seccion dos columnas.

---
+++ cliente/src/components/Dos-columnas/DosColumnas.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import '../../assets/css/styles.css';
+import './DosColumnas.css';
+
+const DosColumnas = () => {
+    return (
+        <Fragment>
+            <section className="contenedor seccion dos-columnas">
+                <div className="row">
+                    <img src={require('../../assets/img/gentecomiendo.png')} />
+                    <div>
+                        <h2 class="fw-300 centrar-texto">El Buen Sabor que satisface tus necesidades</h2>
+                        <p>Amamos la comida tanto como vos y por eso queremos llevar a tu mesa, tu comida favorita directamente desde la cocina del Buen Sabor. ¿La mejor parte? ¡Te la llevamos donde estés!. <br /> Ingresá tu dirección, seleccioná tu comida favorita y listo. ¡Pedir delivery de comida nunca había sido tan sencillo, lo único difícil será decidir qué comer!</p>
+                    </div>
+                </div>
+            </section>
+
+        </Fragment >
+    )
+}
+
+export default DosColumnas
--- cliente/src/components/Home/Home.jsx
+++ cliente/src/components/Home/Home.jsx
@@ -2,13 +2,16 @@ import React, { Fragment } from 'react';
 import Header from '../Header/Header';
 import Nosotros from '../Nosotros/Nosotros';
 import Testimoniales from '../Testimoniales/Testimoniales';
+import DosColumnas from '../Dos-columnas/DosColumnas';
 
 const Home = () => {
     return (
         <Fragment>
             <Header />
             <Nosotros />
+            <DosColumnas />
             <Testimoniales />
+
         </Fragment>
     )
 }
