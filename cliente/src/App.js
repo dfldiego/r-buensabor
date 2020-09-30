099cd73c5c211431213e22ea69a916b490317cea
dfldiego <dfl.diego92@gmail.com>
2020-09-30 17:29:43 -0300

    agregando redux al componente principal. Creando index del reducers.

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 
+// Redux 
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
     <Fragment>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-        </Switch>
+        <Provider store={store}>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
+        </Provider>
       </Router>
     </Fragment>
   );
