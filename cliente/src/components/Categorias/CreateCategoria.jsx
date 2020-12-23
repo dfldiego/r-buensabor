da585196162f50df332e3a4fb8f1fd60259300df
dfldiego <dfl.diego92@gmail.com>
2020-12-23 13:33:55 -0300

    C. Admin. Categorias. parte2. createCategoria. Estilos.

--- cliente/src/components/Categorias/CreateCategoria.jsx
+++ cliente/src/components/Categorias/CreateCategoria.jsx
@@ -1,10 +1,61 @@
 import React, { Fragment } from 'react';
 import './Categorias.css';
 
+// Material Icons
+import ClearIcon from '@material-ui/icons/Clear';
+
+//Actions de Redux
+import {
+    abrirCerrarAgregarCategoriaAction,
+} from '../../actions/adminActions';
+import { useDispatch, useSelector } from 'react-redux';
+
 const CreateCategoria = () => {
+
+    /************USAR DISPATCH: paso el nuevo state al action **********************/
+    const dispatch = useDispatch();
+    const cerrar_modal_callAction = nuevo_estado => dispatch(abrirCerrarAgregarCategoriaAction(nuevo_estado));
+
+    /*************USAR USE SELECTOR: capturo el valor de state del store  *******************/
+    let cerrar_modal_state_store = useSelector(state => state.admin.abrir_agregar_categoria);
+
+    /***********METODO QUE CIERRA MODAL: modifico el state *************/
+    const cerrar_modal = e => {
+        e.preventDefault();
+        if (cerrar_modal_state_store) {
+            cerrar_modal_state_store = false;
+            cerrar_modal_callAction(cerrar_modal_state_store);
+        }
+        return;
+    }
+
     return (
         <Fragment>
-            <h1>CreateCategoria</h1>
+            <div className="modal-categoria">
+                <div className="modal-container-categoria">
+                    <div className="form-container-categoria">
+                        <ClearIcon
+                            className="volver"
+                            onClick={cerrar_modal}
+                        />
+                        <form>
+                            <div className="form-row">
+                                <label>Nombre</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Categoria"
+                                    name="nombre"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                            >Agregar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
         </Fragment>
     )
 }
