91a75976defdfb9b555f916772af250538d08439
dfldiego <dfl.diego92@gmail.com>
2020-12-30 00:15:53 -0300

    C. Catalogo. parte2. estilos y row & column

--- cliente/src/components/Catalogo/Catalogo.jsx
+++ cliente/src/components/Catalogo/Catalogo.jsx
@@ -1,15 +1,31 @@
 import React, { Fragment } from 'react';
+import '../../assets/css/styles.css';
 import './Catalogo.css';
-
 
 import Navbar from '../Navbar/Navbar';
 
 
 const Catalogo = () => {
+
+
+
     return (
         <Fragment>
             <div className="fondo-negro">
                 <Navbar />
+            </div>
+
+            <div class="catalogo seccion contenedor">
+                <div className="row_catalogo">
+                    <div className="col_2_catalogo">
+                        <img src={require('../../assets/img/pizza.jpg')} alt="pizza" />
+                        <h4>Pizza</h4>
+                    </div>
+                    <div className="col_2_catalogo">
+                        <img src={require('../../assets/img/pizza.jpg')} alt="pizza" />
+                        <h4>Pizza</h4>
+                    </div>
+                </div>
             </div>
         </Fragment>
     );
